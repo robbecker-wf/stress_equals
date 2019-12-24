9a65b4d15b20e38c9109bb40b42b6b5ece66f57d
Rob Becker <rob.becker@workiva.com>
2019-12-23 17:17:36 -0700

    added some controls to experiment with

--- gen.dart
+++ gen.dart
@@ -1,5 +1,12 @@
 import 'dart:io';
 
+const bool includeEqualsMethods = false;
+// const bool includeSameAs = true;
+const bool callSameAs = false;
+const bool callEquals = false;
+const int numAdditionalFields = 0;
+const bool useGetterSetters = false;
+
 var classesFile = File('lib/src/classes.dart');
 var mainFile = File('bin/main.dart');
 
@@ -20,16 +27,43 @@ void deleteOldFiles() {
 
 void generateNewFiles(int num) {
   var sb = StringBuffer();
-  sb.writeln('class ZBase {}');
-  for (var i = 0; i < num; i++) {
-    sb.writeln('''class ZBase_$i extends ZBase {
-  int val;
-  ZBase_$i(this.val);
+  sb.writeln('''
+bool sameAs(ZBase a, ZBase b) {
+  print('sameAs called on ZBase');
+  return a.val == b.val;
+}
 
+class ZBase {
+  int val;
+  ZBase(this.val);    
+}
+''');
+  for (var i = 0; i < num; i++) {
+    sb.write('''class ZBase_$i extends ZBase {
+  ZBase_$i(int v''');
+  if (numAdditionalFields > 0) {
+    // sb.write(', {');
+    // for (var i = 0; i < numAdditionalFields; i++) {
+    //   sb.write('this.a$i,');
+    // }
+    // sb.write('}');
+  }
+  sb.writeln('): super(v);');
+
+  for (var i = 0; i < numAdditionalFields; i++) {
+    sb.writeln('    int _a$i;');
+    if (useGetterSetters) {
+      sb.writeln('    int get a$i => _a$i;');
+      sb.writeln('    void set a$i(int x) => _a$i = x;');
+    }
+  }
+  
+  if (includeEqualsMethods) {
+    sb.writeln('''
   @override
   bool operator ==(dynamic other) {
     print('equals called on ZBase_$i');
-    return other is ZBase_$i && val == other.val;
+    return other is ZBase_$i && val == other.val/* && x == other.x*/;
   }
 
   @override
@@ -37,9 +71,12 @@ void generateNewFiles(int num) {
     print('hashCode called on ZBase_$i');
     var value = 17;
     value = (value * 31) ^ val.hashCode;
+    // value = (value * 31) ^ x.hashCode;
   return value;
   }
-
+''');
+  }
+  sb.writeln('''
   @override
   String toString() => 'ZBase_$i(\$val)';
 }
@@ -51,32 +88,44 @@ void generateNewFiles(int num) {
   sb.write('''import 'package:stress_equals/src/classes.dart';
 
 void main() {
-  var s = <ZBase, bool>{};
+  // var s = <ZBase, bool>{};
   print('Creating classes and adding to a map');
 ''');
   for (var i = 0; i < num; i++) {
-    sb.writeln('  s[ZBase_$i($i)] = false;');
+    String assign = 'false;';
+    if (callEquals) {
+      assign = 'ZBase_$i($i,$i) == ZBase_$i($i,$i);';
+    } else if (callSameAs) {
+      assign = 'sameAs(ZBase_$i($i/*,$i*/), ZBase_$i($i/*,$i*/));';
+      sb.writeln(assign);
+    }
+    sb.writeln('ZBase_$i($i);');
+    //sb.writeln('  s[ZBase_$i($i,$i)] = $assign');
+    
   }
   sb.writeln('''
-  var keys = s.keys.toList();
-  var zero = ZBase_$nm1($nm1);
-  print('Calling contains $nm1');
-  if (keys.contains(zero)) {
-    print('It contains a $nm1!');
-  } else {
-    print('No contains $nm1');
-  }
+  // List<ZBase> keys = s.keys.toList();
+  // var last = ZBase_$nm1($nm1,$nm1);
+  // print('Calling contains $nm1');
+  // if (keys.contains(last)) {
+  //   print('It contains a $nm1!');
+  // } else {
+  //   print('No contains $nm1');
+  // }
   
-  print('Getting an object from the map');
-  print('retreived value for key \$zero: \${s[zero]}');
+  // print('Getting an object from the map');
+  // print('retreived value for key \$last: \${s[last]}');
   
-  print('Calling equals on stuff');
+  // print('Calling equals on stuff');
   
-  for (var i = 0; i < keys.length - 1; i++) {
-    if (keys[i] == keys[i+1]) {
-      print('\${keys[i].runtimeType} and \${keys[i+1].runtimeType} are equal and should not be');
-    }
-  }
+  // for (var i = 0; i < keys.length - 1; i++) {
+  //   if (sameAs(keys[i],keys[i+1])) {
+  //     print('SameAs: \${keys[i].runtimeType} and \${keys[i+1].runtimeType} are equal and should not be');
+  //   }
+  //   if (keys[i] == keys[i+1]) {
+  //     print('==: \${keys[i].runtimeType} and \${keys[i+1].runtimeType} are equal and should not be');
+  //   }
+  // }
 }''');
   mainFile.writeAsStringSync(sb.toString());
 }
