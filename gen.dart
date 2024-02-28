62c27d364ba7d62fc9582ffa93c74b450f8c468d
Rob Becker <rob.becker@workiva.com>
2024-02-28 15:33:57 -0700

    stressing over_react

--- gen.dart
+++ gen.dart
@@ -1,84 +1,44 @@
 import 'dart:io';
 
-var classesFile = File('lib/src/classes.dart');
-var mainFile = File('bin/main.dart');
+var componentTemplate = File('web/component.dart.template').readAsStringSync();
+var mainFile = File('web/main.dart');
 
 void main(List<String> args) {
   var num = int.parse(args.isNotEmpty ? args.first : '10');
-  deleteOldFiles();
+  deleteOldFiles(num);
   generateNewFiles(num);
 }
 
-void deleteOldFiles() {
-  try {
-    classesFile.deleteSync();
-  } catch (_) {}
+void deleteOldFiles(int num) {
+  for (var i = 0; i < num; i++) {
+    try {
+      File('web/component$i').deleteSync();
+    } catch (_) {}
+  }
   try {
     mainFile.deleteSync();
   } catch (_) {}
 }
 
 void generateNewFiles(int num) {
-  var sb = StringBuffer();
-  sb.writeln('class ZBase {}');
+  var imports = StringBuffer();
+  var mainCalls = StringBuffer();
+
   for (var i = 0; i < num; i++) {
-    sb.writeln('''class ZBase_$i extends ZBase {
-  int val;
-  ZBase_$i(this.val);
-
-  @override
-  bool operator ==(dynamic other) {
-    print('equals called on ZBase_$i');
-    return other is ZBase_$i && val == other.val;
+    imports.writeln("import 'component${i}.dart' as component${i};");
+    mainCalls.writeln('  component${i}.mane();');
+    var f = File('web/component$i.dart');
+    var out = '$componentTemplate'.replaceAll('XX', '$i');
+    f.writeAsStringSync(out);
   }
+  mainFile.writeAsStringSync('''
+// GENERATED: $num components
+$imports
 
-  @override
-  int get hashCode {
-    print('hashCode called on ZBase_$i');
-    var value = 17;
-    value = (value * 31) ^ val.hashCode;
-  return value;
-  }
-
-  @override
-  String toString() => 'ZBase_$i(\$val)';
+void main() async {
+$mainCalls
 }
 ''');
-  }
-  classesFile.writeAsStringSync(sb.toString());
-  var nm1 = num - 1;
-  sb.clear();
-  sb.write('''import 'package:stress_equals/src/classes.dart';
-
-void main() {
-  var s = <ZBase, bool>{};
-  print('Creating classes and adding to a map');
-''');
-  for (var i = 0; i < num; i++) {
-    sb.writeln('  s[ZBase_$i($i)] = false;');
-  }
-  sb.writeln('''
-  var keys = s.keys.toList();
-  var zero = ZBase_$nm1($nm1);
-  print('Calling contains $nm1');
-  if (keys.contains(zero)) {
-    print('It contains a $nm1!');
-  } else {
-    print('No contains $nm1');
-  }
-  
-  print('Getting an object from the map');
-  print('retreived value for key \$zero: \${s[zero]}');
-  
-  print('Calling equals on stuff');
-  
-  for (var i = 0; i < keys.length - 1; i++) {
-    if (keys[i] == keys[i+1]) {
-      print('\${keys[i].runtimeType} and \${keys[i+1].runtimeType} are equal and should not be');
-    }
-  }
-}''');
-  mainFile.writeAsStringSync(sb.toString());
 }
 
 
